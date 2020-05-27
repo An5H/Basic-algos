389c0d34f43aaf0d2c42984d8d995d5ece07173a
An5H <46046376+An5H@users.noreply.github.com>
2020-05-27 20:00:51 +0530

    Create Implement strStr.java

---
+++ Implement strStr.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+
+class Implement_strstr
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		sc.nextLine();
+		while(t>0)
+		{
+			String line = sc.nextLine();
+			String a = line.split(" ")[0];
+			String b = line.split(" ")[1];
+			
+			GfG g = new GfG();
+			System.out.println(g.strstr(a,b));
+			
+			t--;
+		}
+	}
+}// } Driver Code Ends
+
+class GfG
+{
+    int strstr(String str, String target)
+    {
+        if(str.contains(target)){
+            return str.indexOf(target);
+        }
+        return -1;
+    }
+}
