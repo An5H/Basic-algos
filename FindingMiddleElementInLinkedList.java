ff2fd5b936cc0d1a51e83864e498b76244f19202
An5H <46046376+An5H@users.noreply.github.com>
2020-05-28 19:39:06 +0530

    Create FindingMiddleElementInLinkedList.java

---
+++ FindingMiddleElementInLinkedList.java
@@ -0,0 +1,77 @@
+import java.util.*;
+import java.io.*;
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+    
+}
+class GFG{
+	static void printList(Node node) 
+	{ 
+		while (node != null) 
+		{ 
+			System.out.print(node.data + " "); 
+			node = node.next; 
+		} 
+		System.out.println(); 
+	}
+    public static void main(String args[]) throws IOException { 
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t > 0){
+        	int n = sc.nextInt();
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            for(int i=0; i<n-1; i++)
+            {
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+            Solution g = new Solution();
+            int ans = g.getMiddle(head);
+            System.out.println(ans);
+            //printList(head); 
+            t--;
+        }
+    } 
+} 
+   // } Driver Code Ends
+
+
+/* Node of a linked list
+ class Node {
+   int data;
+    Node next;
+    Node(int d)  { data = d;  next = null; }
+}
+ Linked List class
+class LinkedList
+{
+    Node head;  // head of list
+}
+This is method only submission.  You only need to complete the method. */
+
+
+class Solution
+{
+    // Function to find middle element a linked list
+    int getMiddle(Node head)
+   {    Node a = head;
+        Node b = head;
+        while(b.next != null){
+            b = b.next;
+            a = a.next;
+            if(b.next == null){
+                return a.data;
+            }
+            b = b.next;
+        }
+        return a.data;
+   }
+}
