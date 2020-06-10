ab4d5ab5b533af51e305c34feda98bdd895f3ffe
An5H <46046376+An5H@users.noreply.github.com>
2020-06-10 14:48:50 +0530

    Create DoublyLinkedListInsertionAtGivenPosition.java

---
+++ DoublyLinkedListInsertionAtGivenPosition.java
@@ -0,0 +1,126 @@
+import java.util.*;
+
+class Node
+{
+	int data;
+	Node next;
+	Node prev;
+	Node(int data)
+	{
+	    this.data = data;
+	    next = prev = null;
+	}
+}
+
+class DLinkedList
+{
+	
+	Node newNode(Node head, int data)
+	{
+		Node n = new Node(data);
+		if(head == null)
+		{
+		    head = n;
+		    return head;
+		}
+		head.next = n;
+		n.prev = head;
+		head = n;
+		return head;
+	}
+	
+	
+	void printList(Node node)
+	{
+		Node temp = node;
+		while(temp.next != null)
+		{
+			temp = temp.next;
+		}
+		
+		while(temp.prev != null)
+		{
+			temp = temp.prev;
+		}
+		
+		while(temp != null)
+		{
+			System.out.print(temp.data+" ");
+			temp = temp.next;
+		}
+		System.out.println();
+	}
+	
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		DLinkedList DLL = new DLinkedList();
+		int t = sc.nextInt();
+		
+		while(t>0)
+		{
+			Node temp;
+			Node head = null;
+			Node root = null;
+			int n = sc.nextInt();
+			for(int i=0;i<n;i++)
+			{
+				int x = sc.nextInt();
+				head = DLL.newNode(head,x);
+			    if(root == null)    root = head;
+			}
+			head = root;
+			int pos = sc.nextInt();
+			int data = sc.nextInt();
+			
+		    GfG g = new GfG();
+		    g.addNode(head,pos,data);
+			
+			DLL.printList(head);
+			while(head.next != null)
+			{
+				temp = head;
+				head = head.next;
+			}
+		    t--;
+		}
+	}
+}// } Driver Code Ends
+
+
+/* Structure of Doubly Linked List
+class Node
+{
+	int data;
+	Node next;
+	Node prev;
+	Node(int data)
+	{
+	    this.data = data;
+	    next = prev = null;
+	}
+}*/
+
+class GfG
+{
+void addNode(Node head_ref, int pos, int data)
+	{
+	    if(head_ref == null){
+	        return;
+	    }
+		Node newNode = new Node(data);
+		int count = 0;
+		Node currNode = head_ref;
+		while(count != pos+1 && currNode != null){
+		    ++count;
+		    if(count == pos+1){
+		        Node temp = currNode.next;
+		        currNode.next = newNode;
+		        newNode.prev = currNode;
+		        newNode.next = temp;
+		        break;
+		    }
+		    currNode = currNode.next;
+		}
+	}
+}
