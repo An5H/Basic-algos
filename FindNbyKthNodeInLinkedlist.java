17c687da8855d2a521091cdd07ba6223e08e1a81
An5H <46046376+An5H@users.noreply.github.com>
2020-06-11 15:17:02 +0530

    Create FindNbyKthNodeInLinkedlist.java

---
+++ FindNbyKthNodeInLinkedlist.java
@@ -0,0 +1,99 @@
+
+class Node
+{
+    int data;
+    Node next;
+    
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+class LinkedList
+{
+    static Node head;
+    
+     public static void addToTheLast(Node node) 
+	{
+		if (head == null) 
+		{
+			head = node;
+		} else 
+		{
+		   Node temp = head;
+		   while (temp.next != null)
+		   temp = temp.next;
+		   temp.next = node;
+		}
+    }
+    
+     public static void main (String[] args) {
+         Scanner sc = new Scanner(System.in);
+		 int t=sc.nextInt();
+		 
+		 while(t-- > 0)
+		 {
+		     int n = sc.nextInt();
+		     int a1 = sc.nextInt();
+		     Node head = new Node(a1);
+		     addToTheLast(head);
+		     for(int i = 1; i < n; i++)
+		     {
+		         int a = sc.nextInt(); 
+				 addToTheLast(new Node(a));
+			 }
+			 int k = sc.nextInt();
+			GfG gfg = new GfG();
+			System.out.println(gfg.nknode(head, k));
+		     }
+		 }
+}// } Driver Code Ends
+
+
+
+
+/*
+class Node
+{
+    int data;
+    Node next;
+    
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+*/
+
+class GfG
+{
+    public static int nknode(Node head, int k)
+    {
+        if (k <= 0 || head == null) 
+            return 0; 
+  
+        Node fractionalNode = null; 
+  
+        // Traverse the given list 
+        int i = 0; 
+        for (Node temp = head; temp != null; 
+                          temp = temp.next){ 
+  
+            // For every k nodes, we move 
+            // fractionalNode one step ahead. 
+            if (i % k == 0){ 
+  
+                // First time we see a multiple of k 
+                if (fractionalNode == null) 
+                    fractionalNode = head; 
+                else
+                    fractionalNode = fractionalNode.next; 
+            } 
+            i++; 
+        } 
+        return fractionalNode.data;
+    }
+}
